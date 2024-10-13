8ce6e55defe3d2cc8661f7494254e4b06742d88d
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2024-10-13 18:02:54 +0530

    post method added

--- src/main/java/com/example/HelloWorld/controller.java
+++ src/main/java/com/example/HelloWorld/controller.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -15,13 +18,22 @@ public class controller {
     {
 return "Hello from Home ";
     }
-    
-@GetMapping("calculation")
-public static String getMethodName(@RequestParam int num1, @RequestParam int num2) {
-  //int c = a+b;
+ 
+@GetMapping("calculation") // all value will be passed in url which is unsafe
 
-    return "the result is ";
+public static String getMethodName(@RequestParam int num1, @RequestParam int num2) {
+  int c = num1+ num2 ; //same varaible name as form input name 
+
+    return "the result is "+ c;
 }
+
+
+@PostMapping("securec")
+public static String postMethodName(@RequestParam int a, @RequestParam int b) {
+    int c= a+b;
+    return "secure way" +c;
+}
+
 
     
 }
